bc9c1bb60cf9355c63c24f46fa19d42d46d15f0a
DHAED001 <42849573+DHAED001@users.noreply.github.com>
2019-03-20 16:39:54 -0400

    Adding vnet

--- Stage/main.tf
+++ Stage/main.tf
@@ -38,3 +38,10 @@ tags {
   build-version = "${var.terraform_script_version}"
   }
 }
+resource "azurerm_virtual_network" "web_server_vnet" {
+  name                        = "${var.resource_prefix}-vnet"
+  location                    = "${var.web_server_location}"
+  resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
+  address_space               = ["${var.web_server_address_space}"]
+
+}
